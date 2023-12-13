263227d15a655018480b23592a6d2c2a650db35c
Zannatul Ferdousi Hema <shanzidadisha1234.com>
2023-12-13 18:51:48 +0600

    delete issue  fix

--- index.js
+++ index.js
@@ -106,13 +106,13 @@ async function run() {
     // delete functionality
     app.delete('/cart/:id', async (req, res) => {
       const id = req.params.id
-      const query = { _id: id }
-      const remove = await cartCollection.deleteOne(query);
-      res.send(remove)
+      console.log(id)
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query);
+      res.send(result)
     })
 
-
-
+    
 
   }
   finally {
